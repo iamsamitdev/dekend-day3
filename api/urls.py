ba984cc52cb41376a676ef56627bd3fae54fe9b1
Samit Koyom <samitkoyom@gmail.com>
2024-12-14 17:43:11 +0700

    initial commit

---
+++ api/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from .views import InternViewSet, EducationViewSet, TrainingViewSet, WorkExperienceViewSet,RegisterView, LogoutView
+
+router = DefaultRouter()
+router.register(r'interns', InternViewSet)
+router.register(r'educations', EducationViewSet)
+router.register(r'trainings', TrainingViewSet)
+router.register(r'work-experiences', WorkExperienceViewSet)
+
+urlpatterns = [
+    path('auth/register/', RegisterView.as_view(), name='auth_register'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='auth_login'),
+    path('auth/logout/', LogoutView.as_view(), name='auth_logout'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='auth_refresh'),
+    path('', include(router.urls)), # เพิ่มเส้นทางสำหรับการเข้าถึงข้อมูลทั้งหมดของ router
+]
